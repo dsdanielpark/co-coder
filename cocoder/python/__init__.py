49ff6b7e389cb42c22a8354daa64f8821339b254
parkminwoo <parkminwoo1991@gmail.com>
2023-05-13 07:52:43 +0900

    fix: sys hook in init

--- cocoder/python/__init__.py
+++ cocoder/python/__init__.py
@@ -4,9 +4,6 @@ from cocoder.chatbase.bard_receiver import receive_bard_advice
 from cocoder.chatbase.openai_receiver import receive_openai_advice
 from cocoder.chatbase.openai_receiver import get_resp_openai_advice
 from cocoder.python.core import ExceptPyCocoder
-import sys
-
-sys.excepthook = ExceptPyCocoder.__call__
 
 __version__ = "0.1.1"
 __author__ = "daniel park <parkminwoo1991@gmail.com>"
