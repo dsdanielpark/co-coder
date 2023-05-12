f419c12ad46bfd35ab4697ca49531f4f36ad25ee
parkminwoo <parkminwoo1991@gmail.com>
2023-05-13 07:45:23 +0900

    fix: test.py

--- test.py
+++
@@ -1,8 +0,0 @@
-import cocoder.python
-import os
-os.environ['_BARD_API_KEY'] = 'VwhVqhvFpnOm_sSsLszdru_DWm8otZBt3hPR9fud2BePGTk2jQmGmfcbKb_vyr3IjJFKEA.'
-os.environ["_BARD_ADVICE_LANG"]='ko'
-
-
-print('ssssss')
-print(1/0)
