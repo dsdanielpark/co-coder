904cf7d185eeab4f550ca1c247f163800bb14a68
parkminwoo <parkminwoo1991@gmail.com>
2023-05-13 08:00:34 +0900

    feat: update ipython init

--- cocoder/ipython/__init__.py
+++ cocoder/ipython/__init__.py
@@ -5,7 +5,10 @@ from cocoder.chatbase.bard_receiver import receive_bard_advice
 from cocoder.chatbase.openai_receiver import receive_openai_advice
 from cocoder.chatbase.openai_receiver import get_resp_openai_advice
 
-get_ipython().set_custom_exc((Exception,), ExceptIpyCocoder)
+try:
+    get_ipython().set_custom_exc((Exception,), ExceptIpyCocoder)
+except:
+    pass
 
 __version__ = "0.1.2"
 __author__ = "daniel park <parkminwoo1991@gmail.com>"
