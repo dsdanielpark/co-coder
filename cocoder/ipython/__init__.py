87deef1efa599cb73f0a5bf6a1fb2bef610ad6d9
parkminwoo <parkminwoo1991@gmail.com>
2023-05-14 02:13:42 +0900

    build: 0.1.5 pseudo build for pypi doc

--- cocoder/ipython/__init__.py
+++ cocoder/ipython/__init__.py
@@ -10,6 +10,6 @@ try:
 except:
     pass
 
-__version__ = "0.1.4"
+__version__ = "0.1.5"
 __author__ = "daniel park <parkminwoo1991@gmail.com>"
 __all__ = ["ExceptIpyCocoder", "receive_bard_advice", "receive_openai_advice", "get_resp_openai_advice"]
--- cocoder/python/__init__.py
+++ cocoder/python/__init__.py
@@ -5,6 +5,6 @@ from cocoder.chatbase.openai_receiver import receive_openai_advice
 from cocoder.chatbase.openai_receiver import get_resp_openai_advice
 from cocoder.python.core import ExceptPyCocoder
 
-__version__ = "0.1.4"
+__version__ = "0.1.5"
 __author__ = "daniel park <parkminwoo1991@gmail.com>"
 __all__ = ["ExceptPyCocoder", "receive_bard_advice", "receive_openai_advice", "get_resp_openai_advice"]
