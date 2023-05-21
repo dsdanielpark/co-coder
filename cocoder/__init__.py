1e9b60e10f5751f4eefaab4548c31d616c0efa47
parkminwoo <parkminwoo1991@gmail.com>
2023-05-22 04:52:49 +0900

    refactor: package structure

--- cocoder/__init__.py
+++ cocoder/__init__.py
@@ -1,16 +1,4 @@
 # Copyright 2023 parkminwoo
-from cocoder.aicore.bard_receiver import receive_bard_advice
-from cocoder.aicore.openai_receiver import receive_openai_advice
-from cocoder.aicore.openai_receiver import get_resp_openai_advice
-from cocoder.python.core import ExceptPyCocoder
-from cocoder.ipython.core import except_ipy_cocoder
 
 __version__ = "0.1.5"
 __author__ = "daniel park <parkminwoo1991@gmail.com>"
-__all__ = [
-    "ExceptPyCocoder",
-    "except_ipy_cocoder",
-    "receive_bard_advice",
-    "receive_openai_advice",
-    "get_resp_openai_advice",
-]
