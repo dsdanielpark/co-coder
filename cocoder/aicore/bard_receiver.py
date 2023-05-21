bf37ea903ffa95c1d0d609374a660afdad70b89a
parkminwoo <parkminwoo1991@gmail.com>
2023-05-22 04:38:49 +0900

    feat: update bard language support

--- cocoder/aicore/bard_receiver.py
+++ cocoder/aicore/bard_receiver.py
@@ -1,6 +1,5 @@
-# Copyright 2023 parkminwoo
 import bardapi
-from os import environ
+import os
 
 
 def receive_bard_advice(bard_api_key: str, error_message: str) -> str:
@@ -12,91 +11,47 @@ def receive_bard_advice(bard_api_key: str, error_message: str) -> str:
     :return: Returns a description and example of the code, the location of the error, and a debugging code example.
     :rtype: str
     """
-    environ["_BARD_API_KEY"] = bard_api_key
-
-    if environ.get("_PROMPT_COMMAND") is None:
-        if environ.get("_BARD_ADVICE_LANG") is None:
+    prompt_command = os.environ.get("_PROMPT_COMMAND")
+    bard_advice_lang = os.environ.get("_BARD_ADVICE_LANG")
+    
+    if prompt_command is None:
+        if bard_advice_lang == "ko":
             input_text = (
-                f"Give me more explanation for the following error."
-                f" And if you have a code example and a way to solve it, please suggest it. "
-                f"Please provide as much information as possible. error is {error_message}."
+                "다음 에러와 관련된 더 많은 설명을 알려줘. "
+                "그리고 만약 이 오류와 관련된 코드 예제 및 유용한 자료가 있다면 가능한 많이 알려줘. "
+                f"오류는 {error_message} 입니다."
+            )
+        elif bard_advice_lang == "jp":
+            input_text = (
+                "いくつかのリンクまたはコード例を見つけてください。"
+                "このエラーに関する詳細情報と、この問題に関連する StackOverflow URL を教えてください。 "
+                f"エラーは {error_message} です。"
             )
         else:
-            if environ["_BARD_ADVICE_LANG"] == "ko":
-                input_text = (
-                    f"다음 에러와 관련된 더 많은 설명을 알려줘. "
-                    f"그리고 만약 이 오류와 관련된 코드 예제 및 유용한 자료가 있다면 가능한 많이 알려줘. "
-                    f"오류는 {error_message} 입니다."
-                )
-            elif environ["_BARD_ADVICE_LANG"] == "jp":
-                input_text = (
-                    f"いくつかのリンクまたはコード例を見つけてください。"
-                    f"このエラーに関する詳細情報と、この問題に関連する StackOverflow URL を教えてください。 "
-                    f"エラーは {error_message} です。"
-                )
-            else:
-                print(
-                    "You can only use ko or jp for the _BARD_ADVICE_LANG variable. "
-                    "Hence, answers will be provided in English."
-                )
-                input_text = (
-                    f"Give me more explanation for the following error. "
-                    f"And if you have a code example and a way to solve it, please suggest it. "
-                    f"Please provide as much information as possible. error is {error_message}."
-                )
+            input_text = (
+                "Give me more explanation for the following error. "
+                "And if you have a code example and a way to solve it, please suggest it. "
+                f"Please provide as much information as possible. error is {error_message}."
+            )
     else:
-        input_text = f"{environ['_PROMPT_COMMAND']} error=={error_message}"
+        input_text = f"{prompt_command} error=={error_message}"
 
-    response = bardapi.core.Bard().get_answer(input_text)
-    advice_msg = response["content"]
+    bard_api_key = os.environ.get("_BARD_API_KEY", bard_api_key)
+    bard_lang = os.environ.get("_BARD_ADVICE_LANG", "")
 
-    return advice_msg
+    response = bardapi.core.Bard(token=bard_api_key, language=bard_lang).get_answer(input_text)
+
+    return response["content"]
 
 
-def get_response_bard_advice(bard_api_key: str, error_message: str) -> dict:
+def get_response_bard_advice() -> dict:
     """
-    :param bard_api_key:__Secure-1PSID value of Google bard
-    :type bard_api_key: str
-    :param error_message: Error message
-    :type error_message: str
     :return: Returns a response
     :rtype: str
     """
-    environ["_BARD_API_KEY"] = bard_api_key
+    bard_api_key = os.environ.get("_BARD_API_KEY", "")
+    bard_lang = os.environ.get("_BARD_ADVICE_LANG", "")
 
-    if environ.get("_PROMPT_COMMAND") is None:
-        if environ.get("_BARD_ADVICE_LANG") is None:
-            input_text = (
-                f"Give me more explanation for the following error."
-                f" And if you have a code example and a way to solve it, please suggest it. "
-                f"Please provide as much information as possible. error is {error_message}."
-            )
-        else:
-            if environ["_BARD_ADVICE_LANG"] == "ko":
-                input_text = (
-                    f"다음 에러와 관련된 더 많은 설명을 알려줘. "
-                    f"그리고 만약 이 오류와 관련된 코드 예제 및 유용한 자료가 있다면 가능한 많이 알려줘. "
-                    f"오류는 {error_message} 입니다."
-                )
-            elif environ["_BARD_ADVICE_LANG"] == "jp":
-                input_text = (
-                    f"いくつかのリンクまたはコード例を見つけてください。"
-                    f"このエラーに関する詳細情報と、この問題に関連する StackOverflow URL を教えてください。 "
-                    f"エラーは {error_message} です。"
-                )
-            else:
-                print(
-                    "You can only use ko or jp for the _BARD_ADVICE_LANG variable. "
-                    "Hence, answers will be provided in English."
-                )
-                input_text = (
-                    f"Give me more explanation for the following error. "
-                    f"And if you have a code example and a way to solve it, please suggest it. "
-                    f"Please provide as much information as possible. error is {error_message}."
-                )
-    else:
-        input_text = f"{environ['_PROMPT_COMMAND']} error=={error_message}"
-
-    response = bardapi.core.Bard().get_answer(input_text)
+    response = bardapi.core.Bard(token=bard_api_key, language=bard_lang).get_answer("test_message")
 
     return response
--- cocoder/aicore/openai_receiver.py
+++ cocoder/aicore/openai_receiver.py
@@ -1,12 +1,10 @@
-# Copyright 2023 parkminwoo
 import openai
-from os import environ
+import os
 
 
-def receive_openai_advice(
-    open_ai_model: str, open_ai_api: str, error_message: str
-) -> str:
-    """Receive debugging information about your code from models in open AI.
+def receive_openai_advice(open_ai_model: str, open_ai_api: str, error_message: str) -> str:
+    """
+    Receive debugging information about your code from models in open AI.
 
     :param open_ai_model: model name of open AI
     :type open_ai_model: str
@@ -17,52 +15,39 @@ def receive_openai_advice(
     :return: Returns a description and example of the code, the location of the error, and a debugging code example.
     :rtype: str
     """
-
     openai.api_key = open_ai_api
     model_engine = open_ai_model
 
-    if environ.get("_PROMPT_COMMAND") is None:
+    prompt_command = os.environ.get("_PROMPT_COMMAND")
+    if prompt_command is None:
         input_text = (
-            f"How can I fix this error? Give me short information about next error. "
-            f"Let me know which code line and which code is incorrect. "
+            "How can I fix this error? Give me short information about next error. "
+            "Let me know which code line and which code is incorrect. "
             f"And try to make it fix or fix example. error== {error_message}"
         )
     else:
-        input_text = f"{environ['_PROMPT_COMMAND']} error=={error_message}"
+        input_text = f"{prompt_command} error=={error_message}"
+    
     resp = openai.ChatCompletion.create(
         model=model_engine, messages=[{"role": "user", "content": input_text}]
     )
+    
     advice_msg = resp["choices"][0]["message"]["content"]
     return advice_msg
 
 
-def get_resp_openai_advice(
-    open_ai_model: str, open_ai_api: str, error_message: str
-) -> dict:
-    """Check response of Open AI API status
+def get_resp_openai_advice() -> dict:
+    """
+    Check response of Open AI API status
 
-    :param open_ai_model: model name of open AI
-    :type open_ai_model: str
-    :param open_ai_api: API KEY value of open AI
-    :type open_ai_api: str
-    :param error_message: Error message
-    :type error_message: str
     :return: Returns response dict to openai
     :rtype: dict
     """
-    openai.api_key = open_ai_api
-    model_engine = open_ai_model
-
-    if environ.get("_PROMPT_COMMAND") is None:
-        input_text = (
-            f"How can I fix this error? Give me short information about next error. "
-            f"Let me know which code line and which code is incorrect. "
-            f"and try to make it fix or fix example. error== {error_message}"
-        )
-    else:
-        input_text = f"{environ['_PROMPT_COMMAND']} error=={error_message}"
+    openai.api_key = os.environ.get("_OPEN_AI_API", "")
+    model_engine = os.environ.get("_OPEN_AI_MODEL", "")
+    
     resp = openai.ChatCompletion.create(
-        model=model_engine, messages=[{"role": "user", "content": input_text}]
+        model=model_engine, messages=[{"role": "user", "content": "test"}]
     )
-
+    
     return resp
